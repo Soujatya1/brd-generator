24404fc5b255340209cebc2b789b9e0da46e67e7
Soujatya Das <soujatya548@gmail.com>
2024-11-14 01:29:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,7 @@ model = ChatGroq(groq_api_key="gsk_wHkioomaAXQVpnKqdw4XWGdyb3FYfcpr67W7cAMCQRrNT
 llm_chain = LLMChain(llm=model, prompt=PromptTemplate(
     input_variables=['template_format', 'requirements'],
     template = "Generate a detailed Business Requirements Document (BRD) in the following format: {template_format}. "
-             "For each topics and sub-topics, provide thorough explanations and elaborate on each topic based on these requirements: {requirements}",
-             "Do not hallucinate while elaborating on the topics and sub-topics, remain within the context of the requirements: {requirements}"
+             "For each topics and sub-topics, provide thorough explanations and elaborate on each topic based on these requirements: {requirements}"
 ))
 
 # Streamlit UI
