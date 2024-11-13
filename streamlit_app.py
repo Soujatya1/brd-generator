007687e603ea78e5770ba6a999cd75d6cb51c334
Soujatya Das <soujatya548@gmail.com>
2024-11-13 19:46:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from langchain.prompts import PromptTemplate
 model = ChatGroq(groq_api_key="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", model_name="Llama3-70b-8192")
 
 prompt_template = PromptTemplate(
-    template = f"Generate a Business Requirements Document (BRD) in the following format:\n\n{template_format}\n\nBased on these requirements:\n{requirements}\n",
+    template = "Generate a Business Requirements Document (BRD) in the following format: {template_format} based on these requirements: {requirements}",
     input_variables = ['template_format', 'requirements']
 )
 
