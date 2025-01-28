13b463e190bb948ef7d83af71d7bfcfa7c45b002
Soujatya Das <soujatya548@gmail.com>
2025-01-28 16:31:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,7 @@ model = ChatGroq(
 
 llm_chain = LLMChain(llm=model, prompt=PromptTemplate(
     input_variables=['template_format', 'requirements', 'tables'],
-    template="
-    
-    Create a Business Requirements Document (BRD) based on the following details:
+    template= " Create a Business Requirements Document (BRD) based on the following details:
     
     Document Structure:
     {template_format}
@@ -45,6 +43,8 @@ llm_chain = LLMChain(llm=model, prompt=PromptTemplate(
     Output:
     The output must be formatted cleanly as a Business Requirements Document, following professional standards. Avoid verbose language and stick to the structure defined above.
     "
+    
+    
 ))
 
 st.title("BRD Generator")
