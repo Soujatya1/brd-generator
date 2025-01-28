d60cf1c26d364f5b6e86342ddf39b4ccbc732897
Soujatya Das <soujatya548@gmail.com>
2025-01-28 18:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -187,8 +187,8 @@ if sample_file:
         format_similarity = calculate_structural_similarity(all_tables_as_text, sample_tables)
         st.subheader("Match Score Results")
         if content_similarity_1 != 0:
-            similarity_ratio = content_similarity_1 / content_similarity
-            st.write(f"Similarity Ratio: {similarity_ratio:.2f}")
+            similarity_ratio = (content_similarity_1 / content_similarity)*100
+            st.write(f"Similarity Score: {similarity_ratio:.2f}")
         else:
             print("Error: content_similarity_1 is 0, division by zero is not possible.")
         # Final weighted score
